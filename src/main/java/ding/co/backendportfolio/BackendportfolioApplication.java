8ff9e5a52397de59490e714de77fb1727081de77
kunsanglee <gosmdochee2@gmail.com>
2025-07-12 16:07:08 +0900

    workflow test commit

--- src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
+++ src/main/java/ding/co/backendportfolio/BackendportfolioApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class BackendportfolioApplication {
     public static void main(String[] args) {
-        //test
+        // test
         SpringApplication.run(BackendportfolioApplication.class, args);
     }
 
